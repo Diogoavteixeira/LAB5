1a53e51c5df60f6dc937c4f3fe0b9b8a0c6bfdb8
Diogo Teixeira <A044483@umaia.pt>
2024-11-05 22:43:25 +0000

    exemple3

---
+++ src/main/java/com/bmcl/refactoring/example3/NoDiscount.java
@@ -0,0 +1,11 @@
+package com.bmcl.refactoring.example3;
+
+public class NoDiscount implements Discount {
+
+    @Override
+    public double applyDiscount(double price) {
+        return price;
+    }
+
+
+}
--- src/main/java/com/bmcl/refactoring/example3/SimpleOrder.java
+++ src/main/java/com/bmcl/refactoring/example3/SimpleOrder.java
@@ -1,21 +1,21 @@
 package com.bmcl.refactoring.example3;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class SimpleOrder {
     private Discount discount;
     private double price;
 
     public SimpleOrder(double price) {
         this.price = price;
+        this.discount = new NoDiscount();
     }
-
     public void setDiscount(Discount discount) {
         this.discount = discount;
     }
-
     public double getTotal() {
-        if (discount == null)
-            return price;
-        else
-            return discount.applyDiscount(price);
+        return discount.applyDiscount(price);
     }
 }
